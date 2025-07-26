caab3c91aca4d9bf3d90728f5991f4110bf4a1bd
Carlos Lima <tec.carlos.lima@gmail.com>
2025-07-26 13:48:36 -0300

    Template estatico feito

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,10 +3,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/templates/header/header';
+import { Footer } from './components/templates/footer/footer';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, MatToolbarModule, Header],
+  imports: [RouterOutlet, MatButtonModule, MatToolbarModule, MatCardModule, Header, Footer],
   templateUrl: 'app.componet.html',
 })
 export class App {
