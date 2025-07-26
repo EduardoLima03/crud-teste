f14f7b8cf1ef01e54207801f608589653fbd1e7a
Carlos Lima <tec.carlos.lima@gmail.com>
2025-07-26 15:35:15 -0300

    criando o service do api

---
+++ frontend/src/app/services/http-service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  private baseUrl: string = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+  
+  get<T>(endpoint: string) {
+    return this.http.get<T>(`${this.baseUrl}${endpoint}`);
+  }
+}
