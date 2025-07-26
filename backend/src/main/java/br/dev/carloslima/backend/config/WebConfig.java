c038bb88cf479f64fb7e143313c5986022e32136
Carlos Lima <tec.carlos.lima@gmail.com>
2025-07-26 20:45:48 -0300

    acessando as rotas do backand

---
+++ backend/src/main/java/br/dev/carloslima/backend/config/WebConfig.java
@@ -0,0 +1,4 @@
+package br.dev.carloslima.backend.config;
+
+public class WebConfig {
+}
--- frontend/src/app/views/user/user.ts
+++ frontend/src/app/views/user/user.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { HttpService } from '../../services/http-service';
+import { HttpClient, provideHttpClient} from '@angular/common/http';
 
 @Component({
   standalone:true,
   selector: 'app-user',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
-export class User {
 
+export class User {
+  profiles: any[] = [];
+
+  private httpService = inject(HttpService);
+
+  ngOnInit() {
+    this.httpService.get<any[]>('/permission').subscribe(data => {
+      this.profiles = data;
+      console.log(this.profiles);
+    }, error => {
+      console.error('Error fetching user data', error);
+    });
+  }
 }
