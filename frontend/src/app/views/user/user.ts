a47b9f94c5c2729f73193c4d35929f8a5ae9a4d9
Carlos Lima <tec.carlos.lima@gmail.com>
2025-07-27 07:27:04 -0300

    listando os usuarios

--- frontend/src/app/views/user/user.ts
+++ frontend/src/app/views/user/user.ts
@@ -1,25 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { HttpService } from '../../services/http-service';
-import { HttpClient, provideHttpClient} from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatBadgeModule } from '@angular/material/badge';
+import { ChangeDetectorRef } from '@angular/core';
+import { MatChipListbox } from '@angular/material/chips';
 
 @Component({
   standalone:true,
   selector: 'app-user',
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatChipsModule, MatBadgeModule, MatChipListbox],
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
 
 export class User {
   profiles: any[] = [];
+  users: any[] = [];
+  displayedColumns: string[] = ['name', 'surname', 'email', 'active', 'permissions', 'creationData'];
 
   private httpService = inject(HttpService);
 
+  private cdr = inject(ChangeDetectorRef); // <- injeta ChangeDetectorRef
+
+
   ngOnInit() {
-    this.httpService.get<any[]>('/permission').subscribe(data => {
-      this.profiles = data;
-      console.log(this.profiles);
+    this.httpService.get<any[]>('/users').subscribe(data => {
+      this.users = data;
+      this.cdr.detectChanges();
+      console.log(this.users);
     }, error => {
       console.error('Error fetching user data', error);
     });
